c9c8ab96181ecaa847092de17fc994af05896d24
kimszin <kimszin@naver.com>
2022-05-18 13:39:29 +0900

    third

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Home from '../routes/Home';
+import Detail from '../routes/Detail';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/:id" element={<Detail/>}></Route>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/routes/Home.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+import { connect } from 'react-redux';
+import { actionCreators } from '../Store';
+
+const Home = ({ toDos, addToDo }) => {
+  const [text, setText] = useState("");
+  const onChange = (e) => {
+    setText(e.target.value);
+  }
+  const onSubmit = (e) => {
+    e.preventDefault();
+    addToDo(text);
+    setText("");
+  }
+  return (
+    <>
+      <h1>To Do</h1>
+      <form onSubmit={onSubmit}>
+        <input type="text" value={text} onChange={onChange} />
+        <button>Add</button>
+      </form>
+      <ul>{JSON.stringify(toDos)}</ul>
+    </>
+  )
+}
+
+const mapStateToProps = (state) => {
+  return { toDos: state };
+}
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addToDo: text => dispatch(actionCreators.addToDo(text))
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
